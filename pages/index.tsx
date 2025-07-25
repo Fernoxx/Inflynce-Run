8a61cad9be5f74d9f1649a64865215d80f852914
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-07-26 04:02:57 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,13 +88,6 @@ export default function Home() {
             alt="Window icon"
           />
           Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <Image
             aria-hidden
             src="/globe.svg"
